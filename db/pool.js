13c68d9fe2f9b7c47127feefb38e2d6c02113a4a
oxamyt <dentez.danil@gmail.com>
2024-08-26 08:50:07 +0300

    setup db and tables

---
+++ db/pool.js
@@ -0,0 +1,6 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+
+module.exports = new Pool({
+  connectionString: `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}/${process.env.PGDATABASE}`,
+});
