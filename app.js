c1d35f604510bc659edd18470b8298c98f2a7217
oxamyt <dentez.danil@gmail.com>
2024-09-05 01:58:53 +0300

    add port env for railway

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const pgSession = require("connect-pg-simple")(session);
 require("dotenv").config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
